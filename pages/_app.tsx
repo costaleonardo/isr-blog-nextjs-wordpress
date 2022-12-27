2731d6b863cc5477a0112786d79e96c2907cee66
Leonardo da Costa <costaleonardo97@gmail.com>
2022-12-27 14:33:21 -0500

    Add Bootstrap 5.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import { AppProps } from 'next/app'
+import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/index.css'
 
+import { useEffect } from "react";
+
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return <Component {...pageProps} />
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -16,8 +16,45 @@ export default function Index({ allPosts: { edges }, preview }) {
     <Layout preview={preview}>
       <Head>
         <title>Next.js Blog Example with {CMS_NAME}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Container>
+      <div className="d-flex 
+justify-content-center align-items-center">
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Launch demo modal
+      </button>
+
+      <div
+        className="modal fade"
+        id="exampleModal"
+        // tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">...</div>
+          </div>
+        </div>
+      </div>
+    </div>
         <Intro />
         {heroPost && (
           <HeroPost
